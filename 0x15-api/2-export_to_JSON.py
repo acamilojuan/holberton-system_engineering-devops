2893f8ebcc96f89c5477b4d581db31032ef6ecd5
acamilojuan <1600@holbertonschool.com>
2020-09-14 20:04:50 -0500

    Task 2 attribute name fixed for output

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,8 +6,8 @@ import json
 
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
     res = requests.get(url)
     user_name = res.json().get('username')
     res = requests.get("{}/todos".format(url))
@@ -20,6 +20,6 @@ if __name__ == "__main__":
         new_dic['completed'] = task.get('completed')
         new_dic['username'] = user_name
         new_list.append(new_dic)
-    dic_json[user_id] = new_list
-    with open('{}.json'.format(user_id), 'w') as file:
+    dic_json[id] = new_list
+    with open('{}.json'.format(id), 'w') as file:
         json.dump(dic_json, file)
